9258eeb00fbfe5cac4e3f393e5a841c9b8800965
younke <kewgram@gmail.com>
2020-10-29 11:33:37 +0900

    add bash.local sourcing for bash

--- .config/nixpkgs/extra/init.bash
+++ .config/nixpkgs/extra/init.bash
@@ -1,3 +1,6 @@
 if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi
 
 if [ -e $HOME/.asdf/asdf.sh ]; then . $HOME/.asdf/asdf.sh; fi
+
+if [ -e $HOME/.bash.local ]; then . $HOME/.bash.local; fi
+
